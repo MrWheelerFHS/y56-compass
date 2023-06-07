946da624317b3a7323b6b314499be3e43ab5845e
MrWheelerFHS <115902123+MrWheelerFHS@users.noreply.github.com>
2023-06-07 07:23:12 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,39 @@
+basic.forever(function () {
+    while (input.compassHeading() > 340 || input.compassHeading() < 20) {
+        basic.showLeds(`
+            # . . # .
+            # # . # .
+            # . # # .
+            # . # # .
+            # . . # .
+            `)
+    }
+    while (input.compassHeading() < 200 && input.compassHeading() > 160) {
+        basic.showLeds(`
+            . . # . .
+            . # . # .
+            . . # . .
+            # . . # .
+            . # # . .
+            `)
+    }
+    while (input.compassHeading() < 110 && input.compassHeading() > 70) {
+        basic.showLeds(`
+            . # # # #
+            . # . . .
+            . # # # .
+            . # . . .
+            . # # # #
+            `)
+    }
+    while (input.compassHeading() < 290 && input.compassHeading() > 250) {
+        basic.showLeds(`
+            . # # # #
+            . # . . .
+            . # # # .
+            . # . . .
+            . # # # #
+            `)
+    }
+    basic.showNumber(input.compassHeading())
+})
